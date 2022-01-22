61026fb2b8fd14fc56a666839dd05f3cea72c29e
rjb255 <rjb255@cam.ac.uk>
2022-01-22 17:57:05 +0000

    basix

---
+++ Plantry/mockDesign/atlas/src/App.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+#logo Hello
+
+#nav.flex
+    router-link.navLink(to="/") Home 
+    router-link.navLink(to="/List") Shopping List
+<router-view/>
+</template>
+
+<style lang="sass">
+body
+    background-color: #d0ddd7
+.flex
+    display: flex
+    justify-content: center
+</style>
+<style scoped lang="sass">
+.navLink
+    border: solid
+    border-width: 0px 1px
+    padding: 0 10px
+#logo
+    height:100%
+
+    
+</style>
---
+++ Plantry/mockDesign/atlas/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+#sky
+
+#ground
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="sass" >
+#sky
+
+
+</style>
